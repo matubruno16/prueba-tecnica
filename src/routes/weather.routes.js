c2a78a42fe53a86cf5ed1207bb88650965a2aae1
matubruno16 <matubruno16@gmail.com>
2025-03-25 20:27:16 -0300

    add path /home to serve weather.html

--- src/routes/weather.routes.js
+++ src/routes/weather.routes.js
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { Router } from "express";
 import { WeatherController } from "../controllers/weather.controller.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const router = express.Router();
-
 const weatherController = new WeatherController();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+router.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "weather.html"));
+});
 
 router.get("/weather", (req, res) =>
   weatherController.getWeatherCity(req, res)
